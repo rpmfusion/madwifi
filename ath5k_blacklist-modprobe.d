9299ab2770e1b41beeb89dd14aa7364b2ae5528a
Nicolas Chauvet <kwizart@gmail.com>
2009-04-04 10:01:55 +0000

    dead.package

--- ath5k_blacklist-modprobe.d
+++
@@ -1,8 +0,0 @@
-# This file is part of the madwifi package from livna/rpmfusion
-# You have to consider using ath_pci only if ath5k.ko
-# from newer kernel do not work with your hardware.
-#
-# In this case you have to swich this comment line:
-# ( comment also the  madwifi file  in the same directory).
-blacklist ath5k
-#blacklist ath_pci
--- madwifi-modprobe.d
+++
@@ -1,3 +0,0 @@
-alias wifi0 ath_pci
-alias ath0 ath_pci
-options ath_pci autocreate=sta
